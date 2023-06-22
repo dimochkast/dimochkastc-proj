a688f78152c3eb2ecd9ef5b587d2654d67901ff1
dimochkast <64855319+dimochkast@users.noreply.github.com>
2023-06-22 19:06:32 +0200

    pop

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("input1");
+        string str1 = Console.ReadLine();
+        Console.WriteLine("input2");
+        string str2 = Console.ReadLine();
+
+        int x = Convert.ToInt32(str1);
+        int y = Convert.ToInt32(str2);
+        int sum = x + y;
+        int avg = sum / 2;
+
+        Console.WriteLine($"summa ravna = {sum}");
+        Console.WriteLine($"Averaga ravna = {avg}");
+
+
+
+     
+    
+    
+    
+    }
+}
+
